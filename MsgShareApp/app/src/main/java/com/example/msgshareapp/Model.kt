13d837489e24fdc9a9f5e88818baa83645886760
murarilaliitg <murarilal12051998@gmail.com>
2020-09-09 06:04:49 +0530

    second Commit

---
+++ MsgShareApp/app/src/main/java/com/example/msgshareapp/Model.kt
@@ -0,0 +1,25 @@
+package com.example.msgshareapp
+
+data class Hobby(var title: String)
+object Supplier {
+    val hobbies = listOf<Hobby>(
+        Hobby("Swiming"),
+        Hobby("reading"),
+        Hobby("walking"),
+        Hobby("Sleeping"),
+        Hobby("Programing"),
+        Hobby("Talking"),
+        Hobby("Swiming"),
+        Hobby("reading"),
+        Hobby("walking"),
+        Hobby("Sleeping"),
+        Hobby("Programing"),
+        Hobby("Talking"),
+        Hobby("Swiming"),
+        Hobby("reading"),
+        Hobby("walking"),
+        Hobby("Sleeping"),
+        Hobby("Programing"),
+        Hobby("Talking")
+    )
+}
